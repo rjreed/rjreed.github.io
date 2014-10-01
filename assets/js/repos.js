230b44cf0689fe84cad99e67e73e289965abfaf8
Rocky Reed <rockyjreed@gmail.com>
2014-10-01 11:29:25 -0500

    small changes - switched SVG to use USE

--- assets/js/repos.js
+++ assets/js/repos.js
@@ -2,7 +2,6 @@
   var consts = {
     username: 'rjreed'
   };
-
   var classes = {
     a:  'projects-a',
     li: 'projects-li',
@@ -10,7 +9,6 @@
     lang: 'projects-lang',
     desc: 'projects-desc'
   };
-
   var repoXHR = new XMLHttpRequest();
 
   function repoXHRHandler() {
@@ -19,25 +17,26 @@
 
     for(var i = 0, len = repos.length; i < len; i++){
       if (!repos[i].fork){
-	var li = document.createElement('li');
-	li.className = classes.li;
-	li.innerHTML = '<a class="'
-	  + classes.a
-	  + '" href="'
-	  + repos[i].html_url
-	  + '" target="_blank">'
-	  + repos[i].name
-	  + '<p class="'
-	  + classes.lang
-	  + '"><em>'
-	  + (repos[i].language || 'SVG')
-	  +  '</em></p><p class="'
-	  + classes.desc
-	  + '">'
-	  + repos[i].description
-	  + '</p></a>';
-	
-	ul.appendChild(li);
+				var li = document.createElement('li');
+
+				li.className = classes.li;
+				li.innerHTML = '<a class="'
+					+ classes.a
+					+ '" href="'
+					+ repos[i].html_url
+					+ '" target="_blank">'
+					+ repos[i].name
+					+ '<p class="'
+					+ classes.lang
+					+ '"><em>'
+					+ (repos[i].language || 'SVG')
+					+  '</em></p><p class="'
+					+ classes.desc
+					+ '">'
+					+ repos[i].description
+					+ '</p></a>';
+				
+				ul.appendChild(li);
       }
     }
   }
