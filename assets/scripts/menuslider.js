f2601c460dadbe82daa0a209a5d5cfa994a8de76
rocky reed <rockyjreed@gmail.com>
2020-06-18 06:41:24 -0500

    cleaned workaround out of menuslider

--- assets/scripts/menuslider.js
+++ assets/scripts/menuslider.js
@@ -4,52 +4,54 @@
   This is a function to handle the menu/navbar display / dropdown display.
 */
 
-
 // APP
 (function() {
 
   // SELECTORS
 
-  const selectors = {};
+  window.addEventListener('load', function(event) {
 
-  /// CSS Class Map
-  selectors.hooks = {
-    toggle: "js-dropdown-toggle",
-    dropdown: "js-dropdown",
-    header: "js-header"
-  };
+    const selectors = {};
 
-  /// DOM Nodes
-  const nodes = {
-    header: document.getElementsByClassName(selectors.hooks.header)[0],
-    dropdown: document.getElementsByClassName(selectors.hooks.dropdown)[0]
-  };
+    /// CSS Class Map
+    selectors.hooks = {
+      toggle: "js-dropdown-toggle",
+      dropdown: "js-dropdown",
+      header: "js-header"
+    };
 
-  //// toggle for dropdown visibility
-  let dropdown_visible = false;
+    /// DOM Nodes
+    const nodes = {
+      header: document.getElementsByClassName(selectors.hooks.header)[0],
+      dropdown: document.getElementsByClassName(selectors.hooks.dropdown)[0]
+    };
 
-  //// hack since scrollheight is returning an inconsistent value
-  let dropdown_height =
-    (nodes.dropdown.scrollHeight > 167 ? nodes.dropdown.scrollHeight : 200) + "px";
+    //// toggle for dropdown visibility
+    let dropdown_visible = false;
 
-  //// function to handle clicks on the dropdown toggle node
-  function click_handler(e) {
+    //// hack since scrollheight is returning an inconsistent value
+    // let dropdown_height = (nodes.dropdown.scrollHeight > 167 ? nodes.dropdown.scrollHeight : 200) + "px";
 
-    if (!dropdown_visible && e.target.classList.contains(selectors.hooks.toggle)) {
-      //// show dropdown
-      dropdown_visible = true;
-      nodes.dropdown.style.maxHeight = dropdown_height;
-    } else if (
-      (dropdown_visible && e.target.classList.contains(selectors.hooks.toggle)) ||
-      !nodes.dropdown.contains(e.target)
-    ) {
-      //// hide dropdown
-      dropdown_visible = false;
-      nodes.dropdown.style.maxHeight = 0;
+    let dropdown_height = nodes.dropdown.scrollHeight + 'px';
+
+    //// function to handle clicks on the dropdown toggle node
+    function click_handler(e) {
+
+      if (!dropdown_visible && e.target.classList.contains(selectors.hooks.toggle)) {
+        //// show dropdown
+        dropdown_visible = true;
+        nodes.dropdown.style.maxHeight = dropdown_height;
+      } else if (
+        (dropdown_visible && e.target.classList.contains(selectors.hooks.toggle)) ||
+        !nodes.dropdown.contains(e.target)
+      ) {
+        //// hide dropdown
+        dropdown_visible = false;
+        nodes.dropdown.style.maxHeight = 0;
+      }
     }
-  }
 
-  //// add click_handler event listener to document
-  document.addEventListener("click", click_handler);
-
+    //// add click_handler event listener to document
+    document.addEventListener("click", click_handler);
+  });
 })();
