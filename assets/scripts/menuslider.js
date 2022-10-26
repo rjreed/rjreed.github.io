b913370b8a1c916234214ef5d67528b3cd817005
rjreed <rockyjreed@gmail.com>
2022-10-26 07:07:54 +0000

    updates and modularization

--- assets/scripts/menuslider.js
+++ assets/scripts/menuslider.js
@@ -1,52 +1,56 @@
-// ABOUT
+//// ABOUT
 
 /*
   This is a function to handle the menu/navbar display / dropdown display.
 */
 
-// APP
+//// APP
 (function() {
 
-    // SELECTORS
-    const selectors = {};
+  /// SELECTORS
+  const selectors = {};
 
-    /// CSS Class Map
-    selectors.hooks = {
-      toggle: "js-dropdown-toggle",
-      dropdown: "js-dropdown",
-      header: "js-header"
-    };
+  /// CSS Class Map
+  selectors.hooks = {
+    toggle: ".js-dropdown-toggle",
+    dropdown: ".js-dropdown",
+    header: ".js-header"
+  };
 
-    /// DOM Nodes
-    const nodes = {
-      header: document.getElementsByClassName(selectors.hooks.header)[0],
-      dropdown: document.getElementsByClassName(selectors.hooks.dropdown)[0]
-    };
+  // covenience function to shorten document.querySelector
+  const $qs = (hook) => document.querySelector(hook);
 
-    //// toggle for dropdown visibility
-    let dropdown_visible = false;
+  /// DOM Nodes
+  const nodes = {
+    header: $qs(selectors.hooks.header),
+    dropdown: $qs(selectors.hooks.dropdown)
+  };
 
-    //// function to handle clicks on the dropdown toggle node
-    function click_handler(e) {
+  // toggle for dropdown visibility
+  let dropdown_visible = false;
 
-      if (!dropdown_visible && e.target.classList.contains(selectors.hooks.toggle)) {
-        //// show dropdown
-        dropdown_visible = true;
-        //// set max-height to the scrollHeight of the element
-        /* note: had this variable scoped up but had to wait for window load, this is more consistent with
-         resizing pages, dynamic heights, etc. */
-        nodes.dropdown.style.maxHeight = nodes.dropdown.scrollHeight + 'px';
-      } else if (
-        (dropdown_visible && e.target.classList.contains(selectors.hooks.toggle)) ||
-        !nodes.dropdown.contains(e.target)
-      ) {
-        //// hide dropdown
-        dropdown_visible = false;
-        nodes.dropdown.style.maxHeight = 0;
-      }
+  // function to handle clicks on the dropdown toggle node
+  function click_handler(e) {
+
+    if (!dropdown_visible && e.target.classList.contains(selectors.hooks.toggle.substring(1))) {
+      // show dropdown
+      dropdown_visible = true;
+      // set max-height to the scrollHeight of the element
+      /* note: had this variable scoped up but had to wait for window load, this is more consistent with
+       resizing pages, dynamic heights, etc. */
+      nodes.dropdown.style.maxHeight = nodes.dropdown.scrollHeight + 'px';
     }
+    else if (
+      (dropdown_visible && e.target.classList.contains(selectors.hooks.toggle.substring(1))) ||
+      !nodes.dropdown.contains(e.target)
+    ) {
+      // hide dropdown
+      dropdown_visible = false;
+      nodes.dropdown.style.maxHeight = 0;
+    }
+  }
 
-    //// add click_handler event listener to document
-    document.addEventListener("click", click_handler);
+  // add click_handler event listener to document
+  document.addEventListener("click", click_handler);
 
 })();
